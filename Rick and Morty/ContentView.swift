73ad1b197c91e743fc84441483c1036de0797879
Federico Lupotti <fedelupotti94@gmail.com>
2025-02-11 18:18:44 -0300

    Delete unnecessary code

--- Rick and Morty/ContentView.swift
+++ Rick and Morty/ContentView.swift
@@ -1,7 +1,6 @@
 import SwiftUI
 
 struct ContentView: View {
-    private let homeVM = HomeViewModel()
     var body: some View {
         VStack {
             Image(systemName: "globe")
--- Rick and Morty/HomeViewModel.swift
+++ Rick and Morty/HomeViewModel.swift
@@ -3,14 +3,4 @@ import Foundation
 final class HomeViewModel {
     
     private let apiService = APIService()
-    
-    init() {
-        Task {
-            do {
-                try await apiService.fetchCharacters()
-            } catch {
-                print(error.localizedDescription)
-            }
-        }
-    }
 }
